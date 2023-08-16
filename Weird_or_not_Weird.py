f3715236144c51561bf1350043f9b0efbb829d8d
junnumunnu2004 <121998705+junnumunnu2004@users.noreply.github.com>
2023-08-16 15:10:57 +0530

    n=int(input()) if n%2!= 0:     print("weird") elif n%2 == 0 and n>=2 and n<=5:     print("not weird") elif n%2 == 0 and n>=6 and n<=20:     print("weird") else :     print("not weird")

---
+++ Weird_or_not_Weird.py
@@ -0,0 +1,10 @@
+n=int(input())
+if n%2!= 0:
+    print("weird")
+elif n%2 == 0 and n>=2 and n<=5:
+    print("not weird")
+elif n%2 == 0 and n>=6 and n<=20:
+    print("weird")
+else :
+    print("not weird")
+    
