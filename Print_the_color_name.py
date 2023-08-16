885b4c5f92c8ae3ab8ee045295a01b1bd022612e
junnumunnu2004 <121998705+junnumunnu2004@users.noreply.github.com>
2023-08-16 19:39:32 +0530

    c=input() if c=="V":     print('Violet') elif c=='I':     print('indigo') elif c=='B':     print('Blue') elif c=='G':     print('Green') elif c=='Y':     print('Yellow') elif c=='O':     print('Orange') else:     print('-1')

---
+++ Print_the_color_name.py
@@ -0,0 +1,15 @@
+c=input()
+if c=="V":
+    print('Violet')
+elif c=='I':
+    print('indigo')
+elif c=='B':
+    print('Blue')
+elif c=='G':
+    print('Green')
+elif c=='Y':
+    print('Yellow')
+elif c=='O':
+    print('Orange')
+else:
+    print('-1')
